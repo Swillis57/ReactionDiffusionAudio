0b28027a4481cff57bd8fe9a47bec26a813358f7
sw9662 <sw9662@g.rit.edu>
2017-03-30 03:54:27 -0400

    Center canvas on screen (adaptive) to next lowest power of two from screen height.

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 
 require("./diffusion.js");
 
+//flp2 impl from Hacker's Delight
+function NextLowestPoT(x)
+{
+	x |= x >> 1;
+	x |= x >> 2;
+	x |= x >> 4;
+	x |= x >> 8;
+	x |= x >> 16;
+	return x - (x >> 1);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
 	const cvs = document.querySelector("canvas");
-	cvs.width = 2048;
-	cvs.height = 1024;
+	let bestSize = NextLowestPoT(Math.min(window.innerWidth, window.innerHeight));
+	cvs.width = bestSize;
+	cvs.height = bestSize;
+	cvs.marginLeft = window.innerWidth/4;
 
 	const rd = new ReactionDiffusion(cvs);
 	window.requestAnimationFrame(rd.Run.bind(rd));
